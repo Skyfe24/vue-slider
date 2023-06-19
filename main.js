ba79868cd1bb3445e42464f64a763231e0b25b64
Skyfe24 <fabio.erase@gmail.com>
2023-06-19 17:59:38 +0200

    fixed images

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const app = Vue.createApp({
   data() {
     return {
       images: [
-        { id: 1, url: "img/02.webp" },
+        { id: 1, url: "img/01.webp" },
         { id: 2, url: "img/02.webp" },
         { id: 3, url: "img/03.webp" },
         { id: 4, url: "img/04.webp" },
